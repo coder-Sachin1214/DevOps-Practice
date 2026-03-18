5cbef2d02a2aa41779fc98e9b29ae7504971785c
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2026-03-19 00:29:59 +0530

    feat: introduce continuous deployment workflow and a new web page demonstrating Prisma integration.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,24 +1,9 @@
-import Image, { type ImageProps } from "next/image";
 import { prisma } from "@repo/db/client";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default async function Home() {
+  console.log(process.env.DATABASE_URL);
   const users = await prisma.user.findFirst();
+  console.log("users : ", users);
   return (
     <div>
       <h1>username : {users?.username}</h1>
